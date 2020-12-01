7d2e48565591b676403134b9371355a08615c9f4
Nikhil Vas <nikhilvas123@gmail.com>
2020-12-01 23:57:36 +0530

    Pong-1

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,20 +1,41 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    --     fullscreen = false,
+    --     resizable = false,
+    --     vsync = true
+    -- })
+    love.graphics.setDefaultFilter('nearest','nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
+    push:apply('start')
     love.graphics.printf(
         'Hello Pong!',
         0,
-        WINDOW_HEIGHT / 2 - 6, --Default pixel size = 12
-        WINDOW_WIDTH,
+        VIRTUAL_HEIGHT / 2 - 6, --Default pixel size = 12
+        VIRTUAL_WIDTH,
         'center')
+    push:apply('end')
 end
+
 
