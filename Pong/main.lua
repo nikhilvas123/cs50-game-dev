3edaaea2e64d8760809cfaa2be5eb262b3213d62
Nikhil Vas <nikhilvas123@gmail.com>
2020-12-04 20:02:40 +0530

    Pong-11

--- Pong/main.lua
+++ Pong/main.lua
@@ -27,6 +27,12 @@ function love.load()
     mediumFont = love.graphics.newFont('font.ttf', 16)
     scoreFont = love.graphics.newFont('font.ttf', 32)
 
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static')
+    }
+
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -92,6 +98,8 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            
+            sounds['paddle_hit']:play()
         end
 
         if ball:collides(player2) then
@@ -103,21 +111,29 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+            
+            sounds['paddle_hit']:play()
         end
 
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = - ball.dy
+           
+            sounds['wall_hit']:play()
         end
 
         if ball.y >= VIRTUAL_HEIGHT - ball.width then
             ball.y = VIRTUAL_HEIGHT - ball.width
             ball.dy = - ball.dy
+            
+            sounds['wall_hit']:play()
         end
 
         if ball.x < 0 then
             servingPlayer = 1
             player2Score = player2Score + 1
+            
+            sounds['score']:play()
             
             if player2Score == 10 then
                 winningPlayer = 2
@@ -131,6 +147,8 @@ function love.update(dt)
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
+            
+            sounds['score']:play()
             
             if player1Score == 10 then
                 winningPlayer = 1
