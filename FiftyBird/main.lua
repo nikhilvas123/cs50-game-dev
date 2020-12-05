33eb2edf2a45515e3746de76f9d55a92463b72d6
Nikhil Vas <nikhilvas123@gmail.com>
2020-12-05 14:12:53 +0530

    Bird-6

--- FiftyBird/main.lua
+++ FiftyBird/main.lua
@@ -3,6 +3,7 @@ Class = require 'class'
 
 require 'Bird'
 require 'Pipe'
+require 'PipePair'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -20,10 +21,13 @@ local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 413
+local GROUND_LOOPING_POINT = 514
 
 local bird = Bird()
 local spawnTimer = 0
-local pipes = {}
+local pipePairs = {}
+
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -68,17 +72,23 @@ function love.update(dt)
     spawnTimer = spawnTimer + dt
     
     if spawnTimer > 2 then
-        table.insert(pipes, Pipe())
+        local y = math.max(-PIPE_HEIGHT + 10,
+            math.min(lastY + math.random(-20,20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+        lastY = y
+
+        table.insert(pipePairs, PipePair(y))
         spawnTimer = 0
     end
     
     bird:update(dt)
-    
-    for k, pipe in pairs(pipes) do
-        pipe:update(dt)
 
-        if pipe.x < - pipe.width then
-            table.remove(pipes, k)
+    for k, pair in pairs(pipePairs) do
+        pair:update(dt)
+    end
+
+    for k, pair in pairs(pipePairs) do
+        if pair.remove then
+            table.remove(pipePairs, k)
         end
     end
 
@@ -91,8 +101,8 @@ function love.draw()
 
     love.graphics.draw(background, -backgroundScroll, 0)
     
-    for k, pipe in pairs(pipes) do
-        pipe:render()
+    for k, pair in pairs(pipePairs) do
+        pair:render()
     end
     
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
